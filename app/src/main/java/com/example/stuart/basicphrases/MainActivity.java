26fdc31899efa7c4b0859e3deef9236493d07005
Stuart Kasekende <stuartkasekende1@gmail.com>
2020-10-03 13:57:05 +0100

    First commit

---
+++ app/src/main/java/com/example/stuart/basicphrases/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.stuart.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view)
+    {
+
+        int id = view.getId();
+        String ourId = "";
+
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId,"raw","com.example.stuart.basicphrases");
+        Log.i("Found Resource", String.valueOf(resourceId));
+
+        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
+        mplayer.start();
+
+        Log.i("button tapped", ourId);
+    }
+
+
+}
